42d4277837dafda8fc042071c37ff34c08eaeb27
Yejin9999 <zkdltidl@naver.com>
2025-02-27 11:59:52 +0900

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,7 +1,7 @@
 //웹디자인
 const webSlid = new Swiper('.web_swiper',{
     spaceBetween:80,
-    autoplay:{delay:2500,},
+    autoplay:{delay:3000,},
     loop:true,
     speed:2000,
     navigation: {
@@ -10,6 +10,15 @@ const webSlid = new Swiper('.web_swiper',{
       },
  })
  
+//sns디자인
+const snsSlide = new Swiper('.sns',{
+    autoplay:{delay:0,},
+    loop:true,
+    speed:5000,
+    slidesPerView:4,
+    spaceBetween:20,
+})
+
  //배너디자인
 const bnrSlide = new Swiper('.bnr',{
     autoplay:{delay:0,},
@@ -19,14 +28,6 @@ const bnrSlide = new Swiper('.bnr',{
     spaceBetween:20,
 })
 
-//sns디자인
-const snsSlide = new Swiper('.sns',{
-    autoplay:{delay:0,},
-    loop:true,
-    speed:5000,
-    slidesPerView:4,
-    spaceBetween:20,
-})
 
 //detail디자인
 const detailSlide = new Swiper('.detail',{
@@ -43,24 +44,14 @@ const bnr = document.querySelectorAll('.bnr img')
 const sns = document.querySelectorAll('.sns img')
 const detail = document.querySelectorAll('.detail img')
 
-const body = document.querySelectorAll('body');
+const body = document.querySelector('body');
 console.log(body);
 
 popup_bg.style.display = 'none' //팝업숨기기
 
-for(let i of bnr){
-    i.addEventListener('click',()=>{
-        body.style.overflow = 'hidden';
-        popup_bg.style.display = 'block'
-        popup_bg.children[0].children[0].src = i.src;
-        popup_bg.children[0].style.width = '900px';
-        popup_bg.children[0].style.marginTop = '300px';
-       // console.log(i.src)
-        //console.log(popup_bg.children[0].children[0])
-    })
-}
 for(let i of sns){
     i.addEventListener('click',()=>{
+        body.style.overflow = 'hidden';
         popup_bg.style.display = 'block'
         popup_bg.children[0].children[0].src = i.src;
         popup_bg.children[0].style.width = '700px';
@@ -70,11 +61,25 @@ for(let i of sns){
     })
 }
 
-for(let i of detail){
-    i.addEventListener('click',()=>{
+for(let i of bnr){
+    i.addEventListener('click',()=>{ 
+        body.style.overflow = 'hidden';  
         popup_bg.style.display = 'block'
         popup_bg.children[0].children[0].src = i.src;
-        popup_bg.children[0].style.width = '700px';
+        popup_bg.children[0].style.width = '900px';
+        popup_bg.children[0].style.marginTop = '300px';
+       // console.log(i.src)
+        //console.log(popup_bg.children[0].children[0])
+    })
+}
+
+
+for(let i of detail){
+    i.addEventListener('click',()=>{
+        body.style.overflow = 'hidden';
+        popup_bg.style.display = 'block'
+        popup_bg.children[0].children[0].src = i.src;
+        popup_bg.children[0].style.width = '800px';
         popup_bg.children[0].style.marginTop = '80px';
     })
 }
@@ -85,35 +90,3 @@ popup_bg.addEventListener('click',()=>{
     body.style.removeProperty('overflow');
 })
 
-
-
-/* //전체 화면 수직 마우스 휠
-const wrap = new Swiper('main',{
-    direction:'vertical',
-    mousewheel:true,
-    speed:1000,
-    on: {
-        slideChangeTransitionEnd: function () {
-            for(let i of nav) i.classList.remove('active')
-            nav[this.activeIndex].classList.add('active')
-            setTimeout(() => {
-                ScrollTrigger.refresh(); // ★ Swiper 로드 후 강제 새로고침 ★
-            }, 0);
-        }
-    },
-})
-
-
-//네비 탭 마우스 클릭 시 해당 페이지로 이동하기
-const nav = document.querySelectorAll('nav a')
-console.log(nav)
-
-nav.forEach((t,i)=>{
-    t.addEventListener('click',function(e){
-        e.preventDefault();
-        wrap.slideTo(i, 1000);
-        setTimeout(()=>{
-            ScrollTrigger.refresh();
-        },0);
-    })
-}) */
